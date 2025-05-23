ab0dabff03fcfe2eaade912efe7a37f3850c6330
RoMANzhula <roman1985al@ukr.net>
2025-05-23 23:26:52 +0300

    Add ProductsShopElasticsearchApplication

---
+++ src/main/java/org/romanzhula/products_shop_elasticsearch/ProductsShopElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package org.romanzhula.products_shop_elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsShopElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsShopElasticsearchApplication.class, args);
+	}
+
+}
