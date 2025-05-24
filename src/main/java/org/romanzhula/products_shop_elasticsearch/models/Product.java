0642ea8db2b435bd66f89c44b04afde890eed154
RoMANzhula <roman1985al@ukr.net>
2025-05-24 14:44:43 +0300

    Add Product - model for jpa

---
+++ src/main/java/org/romanzhula/products_shop_elasticsearch/models/Product.java
@@ -0,0 +1,26 @@
+package org.romanzhula.products_shop_elasticsearch.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+
+}
