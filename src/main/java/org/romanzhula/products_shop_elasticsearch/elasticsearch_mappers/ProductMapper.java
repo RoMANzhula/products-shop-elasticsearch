d8ea92e02f4e7e7e61956d3053fe7f1d9105c942
RoMANzhula <roman1985al@ukr.net>
2025-05-25 20:25:14 +0300

    Up ProductMapper - with response and request dto

--- src/main/java/org/romanzhula/products_shop_elasticsearch/elasticsearch_mappers/ProductMapper.java
+++ src/main/java/org/romanzhula/products_shop_elasticsearch/elasticsearch_mappers/ProductMapper.java
@@ -1,11 +1,15 @@
 package org.romanzhula.products_shop_elasticsearch.elasticsearch_mappers;
 
+import org.romanzhula.products_shop_elasticsearch.dto.request.ProductRequest;
+import org.romanzhula.products_shop_elasticsearch.dto.respons.ProductResponse;
 import org.romanzhula.products_shop_elasticsearch.elasticsearch_documents.ProductDocument;
 import org.romanzhula.products_shop_elasticsearch.models.Product;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProductMapper {
 
-    public static ProductDocument toDocument(Product product) {
+    public ProductDocument toDocumentFromEntity(Product product) {
         return ProductDocument.builder()
                 .id(product.getId() != null ? product.getId().toString() : null)
                 .name(product.getName())
@@ -15,12 +19,31 @@ public class ProductMapper {
         ;
     }
 
-    public static Product toEntity(ProductDocument document) {
+    public Product toEntityFromProductRequest(ProductRequest document) {
         return Product.builder()
-                .id(document.getId() != null ? Long.valueOf(document.getId()) : null)
                 .name(document.getName())
                 .description(document.getDescription())
                 .price(document.getPrice())
+                .build()
+        ;
+    }
+
+    public ProductResponse toResponseFromEntity(Product entity) {
+        return ProductResponse.builder()
+                .id(entity.getId() != null ? entity.getId() : null)
+                .name(entity.getName())
+                .description(entity.getDescription())
+                .price(entity.getPrice())
+                .build()
+        ;
+    }
+
+    public ProductResponse toResponseFromDocument(ProductDocument doc) {
+        return ProductResponse.builder()
+                .id(doc.getId() != null ? Long.valueOf(doc.getId()) : null)
+                .name(doc.getName())
+                .description(doc.getDescription())
+                .price(doc.getPrice())
                 .build()
         ;
     }
