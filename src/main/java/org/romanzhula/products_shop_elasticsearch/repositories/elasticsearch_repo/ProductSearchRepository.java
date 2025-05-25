7e772c842e06b00b540fa9baf89853f2cf1d3606
RoMANzhula <roman1985al@ukr.net>
2025-05-25 22:16:15 +0300

    Up ProductSearchRepository - add getByName() for Spring Data Elasticsearch example

--- src/main/java/org/romanzhula/products_shop_elasticsearch/repositories/elasticsearch_repo/ProductSearchRepository.java
+++ src/main/java/org/romanzhula/products_shop_elasticsearch/repositories/elasticsearch_repo/ProductSearchRepository.java
@@ -4,6 +4,12 @@ import org.romanzhula.products_shop_elasticsearch.elasticsearch_documents.Produc
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface ProductSearchRepository extends ElasticsearchRepository<ProductDocument, String> {
+
+    List<ProductDocument> findByName(String name);
+
 }
