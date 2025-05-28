5188b729581013f73df4577768e38babe13be8ec
RoMANzhula <roman1985al@ukr.net>
2025-05-28 19:41:02 +0300

    Up ProductDocument

--- src/main/java/org/romanzhula/products_shop_elasticsearch/elasticsearch_documents/ProductDocument.java
+++ src/main/java/org/romanzhula/products_shop_elasticsearch/elasticsearch_documents/ProductDocument.java
@@ -1,24 +1,34 @@
 package org.romanzhula.products_shop_elasticsearch.elasticsearch_documents;
 
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true) // for ignoring field "_class" (and other unknown fields for our model)
 @Document(indexName = "products")
 public class ProductDocument {
 
     @Id
     private String id;
 
+    @Field(type = FieldType.Text)
     private String name;
+
+    @Field(type = FieldType.Text)
     private String description;
+
+    @Field(type = FieldType.Double)
     private Double price;
 
 }
