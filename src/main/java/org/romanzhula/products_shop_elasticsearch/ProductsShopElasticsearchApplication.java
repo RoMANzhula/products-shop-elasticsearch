e3d834219663b166f59e4316fc77c8a94a32d9c2
RoMANzhula <roman1985al@ukr.net>
2025-05-29 22:58:43 +0300

    Up ProductsShopElasticsearchApplication - add scanBasePackages

--- src/main/java/org/romanzhula/products_shop_elasticsearch/ProductsShopElasticsearchApplication.java
+++ src/main/java/org/romanzhula/products_shop_elasticsearch/ProductsShopElasticsearchApplication.java
@@ -3,7 +3,7 @@ package org.romanzhula.products_shop_elasticsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.romanzhula.products_shop_elasticsearch")
 public class ProductsShopElasticsearchApplication {
 
 	public static void main(String[] args) {
